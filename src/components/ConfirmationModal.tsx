06812a501630ddc17083a769186d754ef86c2e53
Nitiema Allassane <allassanenitiema@gmail.com>
2026-04-14 10:19:52 +0000

    Confirmation modal responsive mode

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -36,7 +36,7 @@ function ConfirmationItem({
                     />
                 </figure>
                 <div>
-                    <h3 className='font-semibold text-(--rose-900) mb-0.5'>{name}u</h3>
+                    <h3 className='font-semibold text-sm sm:text-[16px] text-(--rose-900) mb-0.5'>{name}u</h3>
                     <p className='flex items-center gap-4'>
                         <span className='font-semibold text-(--red)'>{quantity}x</span>
                         <span className='font-medium text-(--rose-500)'>@ ${price}</span>
@@ -53,9 +53,12 @@ function ConfirmationModal({ cartItems, orderTotal, onInitialiseCart}: Confirmat
     return (
         <>
             <div className='fixed top-0 left-0 w-full h-full bg-black/45'>
+
+                {/* Modal bellow */}
                 <article 
-                    className='z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-                    bg-white w-120 p-6 rounded-lg'
+                    className='z-50 absolute bottom-0 left-0 w-full rounded-t-lg
+                    md:bottom-auto md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 
+                    max-h-full md:w-120 md:rounded-lg bg-white p-6'
                 >
                     <div>
                         <figure className='mb-6'>
@@ -74,29 +77,38 @@ function ConfirmationModal({ cartItems, orderTotal, onInitialiseCart}: Confirmat
                             </p>
                         </div>
 
-                        <div className='bg-(--rose-50) p-4 rounded-lg'>
-                            <div className='flex flex-col gap-6 mb-12'>
+                        <div className='bg-(--rose-50) p-4 rounded-lg max-h-60 md:max-h-80 overflow-y-scroll'>
+                            <ul className='flex flex-col mb-12 list-none'>
                                 {cartItems.map(item => (
-                                    <ConfirmationItem  
-                                        thumnail={item.image.thumbnail}
-                                        name={item.name}
-                                        price={item.price}
-                                        quantity={item.quantity}
-                                    />
+                                    <li 
+                                        key={item.name} 
+                                        className='not-last:border-b border-(--rose-100) py-3 md:py-4 last:border-b-none'
+                                    >
+                                        <ConfirmationItem  
+                                            thumnail={item.image.thumbnail}
+                                            name={item.name}
+                                            price={item.price}
+                                            quantity={item.quantity}
+                                        />
+                                    </li>
                                 ))}
-                            </div>
+                            </ul>
 
                             <OrderTotal  
                                 value={orderTotal}
                             />
                         </div>
 
-                        <PrimaryButton 
-                            content="Start New Order" 
-                            onClick={onInitialiseCart}
-                        />
+                        <div className='bg-white pt-6'>
+                            <PrimaryButton 
+                                content="Start New Order" 
+                                onClick={onInitialiseCart}
+                            />
+                        </div>
                     </div>
                 </article>
+
+
             </div>
         </>
     )
